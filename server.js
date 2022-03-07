06955d6692caecffa5dd0511bd61919aa7f382ed
shubhamGupta740 <shubham.a.shubham@accenture.com>
2022-03-07 21:58:47 +0530

    modify server.js file

--- server.js
+++ server.js
@@ -1,25 +1,35 @@
-var bodyParser = require('body-parser');
-var cors = require('cors');
-const express = require('express');
+var bodyParser = require("body-parser");
+var cors = require("cors");
+const express = require("express");
 const app = express();
-
+var LoginService = require("./src/services/login/LoginService").LoginService;
+var SignUpService =
+  require("./src/services/signUp/SignUpService").SignUpService;
+var AddProductService =
+  require("./src/services/addproduct/AddProductService").AddProductService;
+var PushNotificationService =
+  require("./src/croneJobs/PushNotificationService").PushNotificationService;
+var UpdateFBTokenService =
+  require("./src/services/updatefbToken/UpdateFBTokenService").UpdateFBTokenService;
 
 // intialize all the dependencies.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+// import and initialize the service
 
-// import and initialize the service 
-var AddServices = require('./src/services').AddServices;
-new AddServices(app);
+new LoginService(app);
+new SignUpService(app);
+new AddProductService(app);
+new PushNotificationService(app);
+new UpdateFBTokenService(app);
 
-
-app.get("/test",(req,res)=>{
-        res.send("hello")
-})
-const PORT =8080;
-app.listen(PORT,()=>{
-        console.log("Server is running on port", PORT);
-})
+app.get("/test", (req, res) => {
+  res.send("hello");
+});
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
--- src/services/login/LoginService.js
+++ src/services/login/LoginService.js
@@ -1,33 +1,36 @@
 const { STRINGS } = require("../../utils/Strings");
-const {body,validationResult, query} =  require('express-validator');
+const { body, validationResult, query } = require("express-validator");
 const { getErrorModel } = require("../../utils/Utils");
 
 var ROUTES = require("../../api-routes/Routes").ROUTES;
-var LoginServiceProvider = require('./LoginServiceProvider').LoginServiceProvider;
+var LoginServiceProvider =
+  require("./LoginServiceProvider").LoginServiceProvider;
 
+var LoginService = function (app) {
+  let loginServiceProvider = new LoginServiceProvider();
 
+  app.post(
+    ROUTES.LOGIN,
+    body(
+      ["emailId", "password"],
+      STRINGS.PLEASE_SEND_THE_DATA_IN_CORRECT_FORMAT
+    ).exists(),
+    (req, res) => {
+      const errors = validationResult(req);
 
-var LoginService  = function(app){
-    let loginServiceProvider = new LoginServiceProvider();
+      if (!errors.isEmpty()) {
+        return res.status(400).json(getErrorModel(errors.array()));
+      }
 
-    app.post(ROUTES.LOGIN,
-        body(['emailId','password'],STRINGS.PLEASE_SEND_THE_DATA_IN_CORRECT_FORMAT).exists(),
-        (req,res)=>{
-                const errors = validationResult(req);
+      loginServiceProvider.login(function (error, loginResponse) {
+        if (error != null) {
+          res.send(error);
+        } else {
+          res.send(loginResponse);
+        }
+      }, req.body);
+    }
+  );
+};
 
-                if(!errors.isEmpty()){
-                    return res.status(400).json(getErrorModel(errors.array()))
-                }
-                
-                    loginServiceProvider.login(function(error,loginResponse){
-                                if(error!=null){
-                                    res.send(error)
-                                }
-                                else{
-                                    res.send(loginResponse)
-                                }
-                    },req.body)
-        })
-}
-
-exports.LoginService= LoginService;
+exports.LoginService = LoginService;
